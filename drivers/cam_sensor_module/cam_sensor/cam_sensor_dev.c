c12c182184afb8885d7b827c5e8c46c5de1c3d61
Fairphone ODM <fairphone-odm@localhost>
2023-06-28 18:02:04 +0800

    [1 of 1][FP5-162]FP5 camera device info in camera.ko
     ###%%%comment:FP5 camera device info in camera.ko
     ###%%%bug number:FP5-162
     ###%%%product name:FP5
     ###%%%Root Cause:FP5 camera device info in camera.ko
     ###%%%Root Cause Detail:FP5 camera device info in camera.ko
     ###%%%Test_Suggestion:test camera
     ###%%%Test_Report:PASS
     ###%%%Bug category:Platform
     ###%%%Module_Impact:NO
     ###%%%Solution:NO
     ###%%%Pre_build:YES
     ###%%%CodeReviewModule:Camera-system
     ###%%%Highlight:NO
     ###%%%author email:zihaoli@tcl.com
    
    Change-Id: I084dfef724ae3e5966e8a0480a78510b4f4d0782

--- drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
+++ drivers/cam_sensor_module/cam_sensor/cam_sensor_dev.c
@@ -451,6 +451,177 @@ static struct i2c_driver cam_sensor_driver_i2c = {
 	},
 };
 
+/*Begin zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/device.h>
+#include <linux/module.h>
+
+#define show_function(name) \
+    static ssize_t show_##name(struct device *dev,\
+            struct device_attribute *att,\
+            char *buf)\
+{\
+    return snprintf(buf, PAGE_SIZE, "%s\n", name);\
+}\
+
+static struct class *deviceinfo_class = NULL;
+static struct device *deviceinfo_dev = NULL;
+
+unsigned char CamNameB[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamNameB);
+show_function(CamNameB);
+DEVICE_ATTR(CamNameB, 0444, show_CamNameB, NULL);
+
+unsigned char CamNameF[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamNameF);
+show_function(CamNameF);
+DEVICE_ATTR(CamNameF, 0444, show_CamNameF, NULL);
+
+unsigned char CamNameB2[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamNameB2);
+show_function(CamNameB2);
+DEVICE_ATTR(CamNameB2, 0444, show_CamNameB2, NULL);
+
+unsigned char CamOTPB[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamOTPB);
+show_function(CamOTPB);
+
+unsigned char CamOTPF[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamOTPF);
+show_function(CamOTPF);
+
+unsigned char CamOTPB2[128] = "NA:NA:NA";
+EXPORT_SYMBOL(CamOTPB2);
+show_function(CamOTPB2);
+
+static ssize_t CamOTPB_store(struct device *dev,struct  device_attribute *attr,const char *buf,size_t count)
+{
+    int camerab_otp_flag;
+    sscanf(buf,"%d",&camerab_otp_flag);
+    if(camerab_otp_flag==1){
+        printk("otp flag=1\n");
+        sprintf(CamOTPB,"%s","1");
+    }else{
+        printk("otp flag = 0\n");
+        sprintf(CamOTPB,"%s%","0");
+    }
+
+    return count;
+}
+
+static ssize_t CamOTPF_store(struct device *dev,struct  device_attribute *attr,const char *buf,size_t count)
+{
+    int camerab_otp_flag;
+    sscanf(buf,"%d",&camerab_otp_flag);
+    if(camerab_otp_flag==1){
+        printk("otp flag=1\n");
+        sprintf(CamOTPF,"%s","1");
+    }else{
+        printk("otp flag = 0\n");
+        sprintf(CamOTPF,"%s","0");
+    }
+
+    return count;
+}
+
+static ssize_t CamOTPB2_store(struct device *dev,struct  device_attribute *attr,const char *buf,size_t count)
+{
+        int camerab_otp_flag;
+        sscanf(buf,"%d",&camerab_otp_flag);
+
+        if(camerab_otp_flag==1){
+                printk("otp flag=1\n");
+                sprintf(CamOTPB2,"%s","1");
+        }else{
+            printk("otp flag = 0\n");
+            sprintf(CamOTPB2,"%s","0");
+        }
+
+        return count;
+}
+
+DEVICE_ATTR(CamOTPB, 0664, show_CamOTPB, CamOTPB_store);
+DEVICE_ATTR(CamOTPF, 0664, show_CamOTPF, CamOTPF_store);
+DEVICE_ATTR(CamOTPB2, 0664, show_CamOTPB2, CamOTPB2_store);
+
+static struct device_attribute *attr[] = {
+    &dev_attr_CamNameB,
+    &dev_attr_CamNameF,
+    &dev_attr_CamNameB2,
+    &dev_attr_CamOTPB,
+    &dev_attr_CamOTPF,
+    &dev_attr_CamOTPB2,
+    NULL,
+};
+
+static int create_deviceinfo_nodes(void)
+{
+    int ret, i = 0;
+
+    while (attr[i] != NULL) {
+        ret = device_create_file(deviceinfo_dev,  attr[i]);
+        if (ret < 0) {
+            pr_err("Failed to create file for node:%s\n", attr[i]->attr.name);
+            i = i - 1;
+            while (i >= 0) {
+                device_remove_file(deviceinfo_dev, attr[i]);
+                i--;
+            }
+            return -EFAULT;
+        }
+        i++;
+    }
+
+    return 0;
+}
+
+static void remove_deviceinfo_nodes(void)
+{
+    int i = 0;
+
+    while (attr[i] != NULL) {
+        device_remove_file(deviceinfo_dev, attr[i]);
+        i++;
+    }
+}
+
+int deviceinfo_init(void)
+{
+    int ret = -1;
+
+    deviceinfo_class = class_create(THIS_MODULE, "deviceinfo");
+    if (IS_ERR(deviceinfo_class)) {
+        pr_err("Failed to create device info class!\n");
+        return -ENOMEM;
+    }
+
+    deviceinfo_dev = device_create(deviceinfo_class, NULL, 0, NULL,
+                                   "device_info");
+    if (IS_ERR(deviceinfo_dev)) {
+        pr_err("Failed to create device info device!\n");
+        return -ENOMEM;
+    }
+
+    ret = create_deviceinfo_nodes();
+    if (ret) {
+        pr_err("Failed to create all device info nodes!\n");
+        return -EFAULT;
+    }
+
+    return ret;
+}
+
+void deviceinfo_exit(void)
+{
+    remove_deviceinfo_nodes();
+    device_destroy(deviceinfo_class, 0);
+    class_destroy(deviceinfo_class);
+
+    return;
+}
+/*End   zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
+
 int cam_sensor_driver_init(void)
 {
 	int32_t rc = 0;
@@ -461,6 +632,12 @@ int cam_sensor_driver_init(void)
 			rc);
 		return rc;
 	}
+
+	/*Begin zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
+	rc = deviceinfo_init();
+	if(rc)
+		CAM_ERR(CAM_SENSOR, "deviceinfo_init failed rc = %d", rc);
+	/*End   zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
 
 	rc = i2c_add_driver(&cam_sensor_driver_i2c);
 	if (rc)
@@ -473,6 +650,10 @@ void cam_sensor_driver_exit(void)
 {
 	platform_driver_unregister(&cam_sensor_platform_driver);
 	i2c_del_driver(&cam_sensor_driver_i2c);
+
+	/*Begin zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
+	deviceinfo_exit();
+	/*End   zihao.li for [Task][FP5-1587] FP5 camera device info in camera.ko on 20230628*/
 }
 
 MODULE_DESCRIPTION("cam_sensor_driver");
